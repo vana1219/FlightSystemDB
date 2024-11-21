025d6eb01547583fb0f607ad8afc12616c2899ed
vana1219 <71721931+vana1219@users.noreply.github.com>
2024-11-20 21:55:52 -0800

    small fix

--- DAO.js
+++ DAO.js
@@ -7,7 +7,7 @@ class AppDAO {
     this.connection = mysql.createConnection({
       host: '127.0.0.1', // Your database host
       user: 'root', // Your MySQL username
-      password: 'mycomputer', // Your MySQL password
+      password: '', // Your MySQL password
       port: '3306', // Your MySQL port, normally "3306"
       database: 'test', // DO NOT EDIT (You need to create a "test" database from your "MySQL Workbench")
     });
